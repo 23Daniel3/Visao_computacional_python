2c34f29544372a205ee55635a1bf2aa729ef8983
23Daniel3 <daneilcipriani321@gmail.com>
2024-07-20 18:35:30 -0300

    Alterado para sempre que detectar uma pessoa exibir mensagem a cada 3 segundos

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
 from ultralytics import YOLO
+import time
 
 modelo = YOLO('yolov8n.pt')
 
-modelo.predict(source='0',show=True)
+last_detection_time = 0
+detection_interval = 3  
 
+results = modelo.predict(source='0', save=False, show=True, stream=True)
+
+for result in results:
+    mensagem = False
+    current_time = time.time()
+    
+    for box in result.boxes:
+        if box.cls == 0:
+            mensagem = True
+    
+    if mensagem and (current_time - last_detection_time >= detection_interval):
+        print("Pessoa detectada!")
+        last_detection_time = current_time
