a374509808cb6bf8ff9269adf0e6e824f18e44d2
23Daniel3 <daneilcipriani321@gmail.com>
2024-07-22 19:49:59 -0300

    Adicionada função de parada do loop com q e beep quando tem pessoas na frente da câmera

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 from ultralytics import YOLO
 import time
+import winsound
+import keyboard
+import threading
+
+def play_sound():
+    while playing_sound:
+        winsound.Beep(1000, 500)
+
+playing_sound = False
 
 modelo = YOLO('yolov8n.pt')
 
@@ -9,13 +18,25 @@ detection_interval = 3
 results = modelo.predict(source='0', save=False, show=True, stream=True)
 
 for result in results:
+    if keyboard.is_pressed('q'):
+        break
+    
     mensagem = False
     current_time = time.time()
     
     for box in result.boxes:
         if box.cls == 0:
             mensagem = True
-    
-    if mensagem and (current_time - last_detection_time >= detection_interval):
+
+    if mensagem:
         print("Pessoa detectada!")
         last_detection_time = current_time
+        if not playing_sound:
+            playing_sound = True
+            sound_thread = threading.Thread(target=play_sound)
+            sound_thread.start()
+    else:
+        if playing_sound:
+            playing_sound = False
+
+playing_sound = False
